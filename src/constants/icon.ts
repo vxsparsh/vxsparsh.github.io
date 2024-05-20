dfd822f6fae62182de011fb2d72664749a7d9a0f
vxsparsh <sparshanand752@gmail.com>
2024-05-20 12:19:10 +0530

    fix

--- src/constants/icon.ts
+++ src/constants/icon.ts
@@ -2,35 +2,35 @@ import type {Favicon} from "@/types/config.ts";
 
 export const defaultFavicons: Favicon[] = [
     {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'light',
         sizes: '32x32',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'light',
         sizes: '128x128',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'light',
         sizes: '180x180',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'light',
         sizes: '192x192',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'dark',
         sizes: '32x32',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'dark',
         sizes: '128x128',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'dark',
         sizes: '180x180',
     }, {
-        src: '/src/assets/images/favicon.png',
+        src: '/favicon/favicon.png',
         theme: 'dark',
         sizes: '192x192',
     }
